4b8d37e25b79c28b9aea7ecb679bced7458ac7db
Carol <carolchristovamc@gmail.com>
2023-09-01 19:45:43 -0300

    error corrections

--- app/src/main/java/com/example/restaurantjetpackcompose/list_item/FoodItem.kt
+++ app/src/main/java/com/example/restaurantjetpackcompose/list_item/FoodItem.kt
@@ -2,25 +2,34 @@ package com.example.restaurantjetpackcompose.list_item
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.restaurantjetpackcompose.R
+import java.nio.file.WatchEvent
 
 @Composable
-fun FoodItem (
+fun FoodItem(
 
-){
+) {
 
     ConstraintLayout(
         modifier = Modifier
@@ -30,41 +39,124 @@ fun FoodItem (
     ) {
 
         val (
-        imgFood,
-        txtFoodName,
-        txtFoodDescription,
-        txtPrice,
-        btAddCart,
-        containerImg,
-        rowItem
+            imgFood,
+            txtFoodName,
+            txtFoodDescription,
+            txtPrice,
+            btAddCart,
+            containerImg,
+            rowItem
         ) = createRefs()
 
-        Card (
+        Card(
             modifier = Modifier
                 .constrainAs(containerImg) {
-
+                    top.linkTo(parent.top, 0.dp)
+                    start.linkTo(parent.start, 0.dp)
                 }
-                .size(30.dp)
+                .size(130.dp)
                 .padding(20.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color.White
-                ),
-                shape = ShapeDefaults.Medium,
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 8.dp
-                )
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            shape = ShapeDefaults.Medium,
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 8.dp
+            )
 
+        ) {
+
+        }
+
+        Image(
+            painter = painterResource(id = R.drawable.food1),
+            contentDescription = null,
+            modifier = Modifier
+                .constrainAs(imgFood) {
+                    top.linkTo(containerImg.top, 0.dp)
+                    start.linkTo(containerImg.start, 0.dp)
+                    end.linkTo(containerImg.end, 0.dp)
+                    bottom.linkTo(containerImg.bottom, 0.dp)
+                }.size(60.dp)
+        )
+
+        Text(
+            text = "Traditional Italian Pizza",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.constrainAs(txtFoodName) {
+                top.linkTo(parent.top, 30.dp)
+                start.linkTo(containerImg.end, 0.dp)
+            }
+        )
+
+        Text(
+            text = "Buffalo mozzarella and tomato sauce",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier
+                .constrainAs(txtFoodDescription) {
+                    top.linkTo(txtFoodName.bottom)
+                    start.linkTo(containerImg.end)
+                    end.linkTo(parent.end)
+                }
+                .padding(65.dp, 10.dp, 40.dp, 0.dp),
+            maxLines = 3
+        )
+
+        Text(
+            text = "$ 25.00",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.constrainAs(txtPrice) {
+                top.linkTo(txtFoodDescription.bottom, 20.dp)
+                start.linkTo(containerImg.end, 0.dp)
+            }
+        )
+
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier
+                .height(45.dp)
+                .constrainAs(btAddCart) {
+                    top.linkTo(txtFoodDescription.bottom, 10.dp)
+                    start.linkTo(txtPrice.end, 10.dp)
+                    end.linkTo(parent.end, 10.dp)
+                },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFF44336)
+            )
+        ) {
+            Text(
+                text = "Add Cart",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color= Color.White
+            )
+        }
+        
+        Row (
+            modifier = Modifier.width(350.dp).height(3.dp).background(Color.White)
+                .constrainAs(rowItem){
+                    top.linkTo(txtPrice.bottom, 30.dp)
+                    start.linkTo(parent.start,20.dp)
+                    end.linkTo(parent.end,20.dp)
+                }
                 ){
 
         }
-        Image(
-            painter = painterResource(id = R.drawable.food1 ),
-            contentDescription = )
+
+
     }
 }
 
 @Composable
 @Preview
-private fun FoodItemPreview (){
+private fun FoodItemPreview() {
     FoodItem()
 }
